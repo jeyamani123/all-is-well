602b06037ba0c58af0e346b0ddeef44489c5aacb
jeyamani123 <43089543+jeyamani123@users.noreply.github.com>
2020-02-04 13:24:27 +0530

    Add files via upload

---
+++ js/home/stringsamelen.js
@@ -0,0 +1,11 @@
+function isSameLength(word1, word2){
+    if((word1.length) ==(word2.length))
+    {
+        alert(true);
+    }
+    else
+      alert(false);
+   }
+word=prompt("enter a word");
+wo=prompt("enter a word");
+isSameLength(word,wo);

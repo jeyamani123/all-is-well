c5d9fc776843e85e166b1857c0482a9a906fd344
jeyamani123 <43089543+jeyamani123@users.noreply.github.com>
2020-02-04 13:05:15 +0530

    Add files via upload

---
+++ js/home/odd.js
@@ -0,0 +1,11 @@
+function areBothOdd(num1, num2){
+    if(num1%2==0 && num2%2==0)
+    {
+        alert(false);
+    }
+    else
+       alert(true);
+   }
+num1=Number(prompt("enter a value"));
+num2=Number(prompt("enter a value2"));
+areBothOdd(num1,num2);

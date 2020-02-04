de45cfc7417bff1938047c090dbf6b8b66e9ecf9
jeyamani123 <43089543+jeyamani123@users.noreply.github.com>
2020-02-04 13:15:54 +0530

    Add files via upload

---
+++ js/home/string.js
@@ -0,0 +1,7 @@
+function getFullName(firstName, lastName){
+    var a=firstName+" "+lastName;
+    alert(a);
+   }
+first=prompt("Enter a first name:");
+last=prompt("Enter a last name:");
+getFullName(first,last);

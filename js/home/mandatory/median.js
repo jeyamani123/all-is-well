410d06dcfa83d60be59189d6d6411e9e64896eb4
jeyamani123 <43089543+jeyamani123@users.noreply.github.com>
2020-02-05 23:41:20 +0530

    Update median.js

--- js/home/mandatory/median.js
+++ js/home/mandatory/median.js
@@ -1,8 +1,8 @@
-var a=function (b,m){
+var a=function (b,m,n){
     var d=b+m;
     var c=((Number(d[6])+Number(d[7]))/2);
     console.log(c);
 }
 var o=[1,2,3,4];
 var b=[6,7,8,9];
-a(o,b);
+a(o,b,n);

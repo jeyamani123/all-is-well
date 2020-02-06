249daa36c69193623092aded8db6aaaeca652a0d
jeyamani123 <43089543+jeyamani123@users.noreply.github.com>
2020-02-06 21:20:40 +0530

    Add files via upload

---
+++ js/home/json/model/json2.js
@@ -0,0 +1,7 @@
+var obj = {name : "RajiniKanth", age : 33, hasPets : false};
+printAllValues(obj);
+function printAllValues(obj) {
+            document.write( Object.values(obj));
+            
+ 
+}
